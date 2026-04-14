d69ea4182a9af6c6ee06859a73692c464abc9d7b
xfoliux <cristiangualan91@gmail.com>
2026-04-13 22:53:48 -0500

    se agrega las funciones moverIzquierda y moverDerecha

--- limones.js
+++ limones.js
@@ -4,6 +4,8 @@ const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
+let personajeX=canvas.width/2;
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -17,5 +19,25 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle = "yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarPersonaje();
+    dibujarSuelo();
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
 }

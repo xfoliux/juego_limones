ce473e3b4f444af5e7b231d5c1f922ba0cd5b46e
xfoliux <cristiangualan91@gmail.com>
2026-04-13 23:09:06 -0500

    se agrega la funcion agregarLimon

--- limones.js
+++ limones.js
@@ -3,12 +3,16 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
-
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
 let personajeX=canvas.width/2;
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 
 }
 
@@ -30,6 +34,7 @@ function actualizarPantalla(){
     limpiarCanvas();
     dibujarPersonaje();
     dibujarSuelo();
+    dibujarLimon();
 }
 
 function moverIzquierda(){
@@ -39,5 +44,15 @@ function moverIzquierda(){
 
 function moverDerecha(){
     personajeX=personajeX+10;
+    actualizarPantalla();
+}
+
+function dibujarLimon(){
+    ctx.fillStyle = "green";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+}
+
+function bajarLimon(){
+    limonY=limonY+10;
     actualizarPantalla();
 }

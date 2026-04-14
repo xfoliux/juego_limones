efe1ba8a40dc6aeec55e996b3150fe9a524c5def
xfoliux <cristiangualan91@gmail.com>
2026-04-13 22:33:56 -0500

    agregar dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,2 +1,21 @@
 let canvas=document.getElementById("areaJuego");
-let ctx=canvas.getContex("2d");
+let ctx=canvas.getContext("2d");
+const ALTURA_SUELO=20;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+
+function dibujarSuelo(){
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width, ALTURA_SUELO);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
